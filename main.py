e621d09a4432d4aba96f4b1a0aa40b82cb161e41
Lycaeum <ebanoac@gmail.com>
2019-10-05 10:21:20 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# FarmaCAPS:    Sistema de Gerenciamento de Medicamentos Controlados
+#               do CAPSII Lauro de Freitas
+#
+# AUTOR: Ebano Augusto de Castro
+# LICENSA: GPLv3
+
+if __name__ == "__main__":
+    def main():
+        pass
